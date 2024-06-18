3539ebfe72925810fe37d1b6fae295f5daf16409
Billy <billy.billio.bones@gmail.com>
2024-06-19 00:48:43 +0200

    ✨ F02 : ErrorPage > Redirection

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+        ErrorPage
+        <Link to="/" className="w-15 h-11 rounded-lg bg-green-400 text-white flex justify-center items-center">
+            Revenir à la page d'accueil
+        </Link>
+    </div>
   )
 }
--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ export default function LandingPage() {
   return (
     <div className='flex justify-center items-center h-screen'>
          <Link to="/login" className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white font-bold py-4 px-8 rounded-full shadow-lg transform transition duration-500 hover:scale-110 hover:shadow-2xl">
-        Déconnexion
+        S'inscrire
         </Link>
     </div>
   )
