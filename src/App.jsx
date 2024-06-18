c75e15733f5f23479990fd0a9f3f964fd092afe8
Billy <billy.billio.bones@gmail.com>
2024-06-19 01:29:21 +0200

    ✨ F02 : Dashboard > création de la page + navigate avec des paramètres

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import { Route, Routes } from 'react-router-dom'
 import ErrorPage from './components/pages/ErrorPage'
 import Logo from './components/Logo'
 import LandingPage from './components/pages/LandingPage'
+import Dashboard from './components/pages/Dashboard'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage/>} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard/:username" element={<Dashboard/>} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   )
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Logo from './Logo';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Form = () => {
 
   const [username, setUsername] = useState(''); // état pour le nom d'utilisateur
   const [password, setPassword] = useState(''); // état pour le mot de passe
+  const navigate = useNavigate()
 
   const handleChangeUsername = (event) => { 
     setUsername(event.target.value)  
@@ -14,7 +15,7 @@ const Form = () => {
     setPassword(event.target.value)  
   }
   const handleClick = () => {
-    alert(`Bonjour ${username}`)
+    navigate(`/dashboard/${username}`)
   }
 
   return (
@@ -22,14 +23,14 @@ const Form = () => {
         <Logo></Logo>
         <form action="submit" method="get" className="w-full flex flex-col">
           <div className="form-example mb-4">
-            <label htmlFor="name" className="block text-gray-700">Entrez votre mail:</label>
+            <label htmlFor="name" className="block text-gray-700">Entrez votre nom:</label>
             <input type="text" name="name" id="name" required="required" 
                    className="mt-1 p-2 border rounded w-full"
                    onChange={handleChangeUsername}/>
           </div>
 
           <div className="form-example mb-4">
-            <label htmlFor="email" className="block text-gray-700">Entrez votre mot de passe:</label>
+            <label htmlFor="email" className="block text-gray-700">Entrez votre mail:</label>
             <input type="email" name="email" id="email" required="required" 
                    className="mt-1 p-2 border rounded w-full"
                    onChange={handleChangePassword}/>
