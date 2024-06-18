2b05df668df4d9a63118e2f6bcd82a4f261645c9
Billy <billy.billio.bones@gmail.com>
2024-06-18 22:07:08 +0200

    ✨ F02 : Routes Browser

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 import LoginPage from './components/pages/LoginPage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <LoginPage></LoginPage>
-    </>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      {/*<Route path="*" element={<NotFoundPage />} />*/}
+    </Routes>
   )
 }
 
-export default App
+export default App;
